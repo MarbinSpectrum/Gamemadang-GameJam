56f8212e8069fec7368be97ce6a5339fe7a9b784
KangYoungMook <9507ym@naver.com>
2024-11-23 04:57:22 +0900

    change sync

--- GameMadang/Assets/Scripts/Ymk/InGame/InGameSync.cs
+++ GameMadang/Assets/Scripts/Ymk/InGame/InGameSync.cs
@@ -4,15 +4,146 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 
-public class InGameSync : MonoBehaviourPun, IPunObservable
+public class InGameSync : MonoBehaviourPunCallbacks
 {
-    public int          gameSeed;
-    public GameResult   res;
-    public int          masterHp;
-    public int          slaveHp;
-    public int          round;
-    public int          masterWin;
-    public int          slaveWin;
+    private int GameSeed;
+    public int gameSeed
+    {
+        get
+        {
+            return GameSeed;
+        }
+        set
+        {
+            GameSeed = value;
+
+            ExitGames.Client.Photon.Hashtable hashtable = new ExitGames.Client.Photon.Hashtable();
+            hashtable.Add("gameSeed", gameSeed);
+
+            Player[] player = PhotonNetwork.PlayerListOthers;
+            foreach (Player p in player)
+                p.SetCustomProperties(hashtable);
+        }
+    }
+    private int Round;
+    public int round
+    {
+        get
+        {
+            return Round;
+        }
+        set
+        {
+            Round = value;
+
+            ExitGames.Client.Photon.Hashtable hashtable = new ExitGames.Client.Photon.Hashtable();
+            hashtable.Add("round", round);
+
+            Player[] player = PhotonNetwork.PlayerListOthers;
+            foreach (Player p in player)
+                p.SetCustomProperties(hashtable);
+        }
+    }
+
+    private GameResult Res;
+    public GameResult res
+    {
+        get
+        {
+            return Res;
+        }
+        set
+        {
+            Res = value;
+
+            ExitGames.Client.Photon.Hashtable hashtable = new ExitGames.Client.Photon.Hashtable();
+            hashtable.Add("res", res);
+
+            Player[] player = PhotonNetwork.PlayerListOthers;
+            foreach (Player p in player)
+                p.SetCustomProperties(hashtable);
+        }
+    }
+
+    private int MasterHp;
+    public int masterHp
+    {
+        get
+        {
+            return MasterHp;
+        }
+        set
+        {
+            MasterHp = value;
+
+            ExitGames.Client.Photon.Hashtable hashtable = new ExitGames.Client.Photon.Hashtable();
+            hashtable.Add("masterHp", masterHp);
+
+            Player[] player = PhotonNetwork.PlayerListOthers;
+            foreach (Player p in player)
+                p.SetCustomProperties(hashtable);
+        }
+    }
+
+    private int SlaveHp;
+    public int slaveHp
+    {
+        get
+        {
+            return SlaveHp;
+        }
+        set
+        {
+            SlaveHp = value;
+
+            ExitGames.Client.Photon.Hashtable hashtable = new ExitGames.Client.Photon.Hashtable();
+            hashtable.Add("slaveHp", slaveHp);
+
+            Player[] player = PhotonNetwork.PlayerListOthers;
+            foreach (Player p in player)
+                p.SetCustomProperties(hashtable);
+        }
+    }
+
+    private int MasterWin;
+    public int masterWin
+    {
+        get
+        {
+            return MasterWin;
+        }
+        set
+        {
+            MasterWin = value;
+
+            ExitGames.Client.Photon.Hashtable hashtable = new ExitGames.Client.Photon.Hashtable();
+            hashtable.Add("masterWin", masterWin);
+
+            Player[] player = PhotonNetwork.PlayerListOthers;
+            foreach (Player p in player)
+                p.SetCustomProperties(hashtable);
+        }
+    }
+
+    private int SlaveWin;
+    public int slaveWin
+    {
+        get
+        {
+            return SlaveWin;
+        }
+        set
+        {
+            SlaveWin = value;
+
+            ExitGames.Client.Photon.Hashtable hashtable = new ExitGames.Client.Photon.Hashtable();
+            hashtable.Add("slaveWin", slaveWin);
+
+            Player[] player = PhotonNetwork.PlayerListOthers;
+            foreach (Player p in player)
+                p.SetCustomProperties(hashtable);
+        }
+    }
 
     public void Start()
     {
@@ -23,30 +154,59 @@ public class InGameSync : MonoBehaviourPun, IPunObservable
         }
     }
 
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
     {
-        if (stream.IsWriting)
+        if (targetPlayer == PhotonNetwork.LocalPlayer)
         {
-            //값을 날려주는 부분
-            stream.SendNext(gameSeed);
-            stream.SendNext((int)res);
-            stream.SendNext(masterHp);
-            stream.SendNext(slaveHp);
-            stream.SendNext(round);
-            stream.SendNext(masterWin);
-            stream.SendNext(slaveWin);
-        }
-        else
-        {
-            //값을 가져오는 부분
-            this.gameSeed = (int)stream.ReceiveNext();
-            this.res = (GameResult)stream.ReceiveNext();
-            this.masterHp = (int)stream.ReceiveNext();
-            this.slaveHp = (int)stream.ReceiveNext();
-            this.round = (int)stream.ReceiveNext();
-            this.masterWin = (int)stream.ReceiveNext();
-            this.slaveWin = (int)stream.ReceiveNext();
+            if (changedProps["gameSeed"] != null)
+            {
+                GameSeed = (int)changedProps["gameSeed"];
+
+                string[] checkProperties = new string[] { "gameSeed" };
+                PhotonNetwork.RemovePlayerCustomProperties(checkProperties);
+            }
+            else if (changedProps["round"] != null)
+            {
+                Round = (int)changedProps["round"];
+
+                string[] checkProperties = new string[] { "round" };
+                PhotonNetwork.RemovePlayerCustomProperties(checkProperties);
+            }
+            else if (changedProps["res"] != null)
+            {
+                Res = (GameResult)changedProps["res"];
+
+                string[] checkProperties = new string[] { "res" };
+                PhotonNetwork.RemovePlayerCustomProperties(checkProperties);
+            }
+            else if (changedProps["masterHp"] != null)
+            {
+                MasterHp = (int)changedProps["masterHp"];
+
+                string[] checkProperties = new string[] { "masterHp" };
+                PhotonNetwork.RemovePlayerCustomProperties(checkProperties);
+            }
+            else if (changedProps["slaveHp"] != null)
+            {
+                SlaveHp = (int)changedProps["slaveHp"];
+
+                string[] checkProperties = new string[] { "slaveHp" };
+                PhotonNetwork.RemovePlayerCustomProperties(checkProperties);
+            }
+            else if (changedProps["masterWin"] != null)
+            {
+                MasterWin = (int)changedProps["masterWin"];
+
+                string[] checkProperties = new string[] { "masterWin" };
+                PhotonNetwork.RemovePlayerCustomProperties(checkProperties);
+            }
+            else if (changedProps["slaveWin"] != null)
+            {
+                SlaveWin = (int)changedProps["slaveWin"];
+
+                string[] checkProperties = new string[] { "slaveWin" };
+                PhotonNetwork.RemovePlayerCustomProperties(checkProperties);
+            }
         }
     }
 
