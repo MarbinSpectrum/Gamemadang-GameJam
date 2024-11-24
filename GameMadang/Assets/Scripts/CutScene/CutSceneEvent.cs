8549ddff975b7d4e0c2055e94c66870c4d368600
감자튀김 <93758339+diddmstn@users.noreply.github.com>
2024-11-24 16:45:30 +0900

    Add : CutScene

--- GameMadang/Assets/Scripts/CutScene/CutSceneEvent.cs
+++ GameMadang/Assets/Scripts/CutScene/CutSceneEvent.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -6,6 +7,12 @@ public class CutSceneEvent : MonoBehaviour
     //넣을 수도 있는거 효과음, 움직임, 대사변환 정도?
 
     private TextMeshProUGUI text;
+    private string[] dialogue = { "받아라 정상화의 총알!", "거기 너! 숨어봤자 소용없다!","이제 그만 너희 세계로 돌아가!","이세계 정상화!" };
+
+    [SerializeField] private GameObject explosionVFX;
+    [SerializeField] private GameObject shootVFX;
+
+    Sequence sequence;
 
     private void Awake()
     {
@@ -20,12 +27,21 @@ public class CutSceneEvent : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+    //이펙트를 하나로 합치고 
+    public void ShootSFX()
+    {
+        //GameObject Shoo = Instantiate(shootVFX, ); 잠깐보류
 
+        sequence = DOTween.Sequence()
+       .Append(explosionVFX.transform.DOMove(Input.mousePosition, 1f).SetUpdate(true))
+       .Append(DOTween.To(() => 0f, x => { explosionVFX.SetActive(false); },1f,0f)).SetUpdate(true)
+       .Join(DOTween.To(() => 0f, x => { shootVFX.SetActive(true); },1f,0f).SetUpdate(true));
+    }
 
 
     private void RandomText()
     {
-        text.text = "지금은 대사 없어요";
+        text.text = dialogue[Random.Range(0,dialogue.Length)];
     }
 
     
